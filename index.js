10d7d4fccc0b3585d905e200de8a326f9608c77f
Adeline Almanzar <almanzar.adeline@gmail.com>
2021-12-19 17:01:02 -0500

    Compelted assignment

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
-// Code your solutions in this file
+let names = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(namesvar, eventname) {
+    let array = [];
+    for (let i = 0; i < namesvar.length; i++) {
+        array.push(`Thank you, ${namesvar[i]}, for the wonderful ${eventname} gift!`);
+    }
+    return array;
+};
+writeCards(
+    names,
+    "surprise",
+);
+
+function countDown(max) {
+    while (max >= 0) {
+        console.log(max);
+        max--;
+    }
+};
+
+countDown(10);
